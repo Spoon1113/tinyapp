265c04952a5cfb559fac0c477c95c7f19bab4b6a
Samson Poon <samsonkfpoon@gmail.com>
2023-01-24 03:31:38 +0000

    Added redirect based on short and long url ids

--- express_server.js
+++ express_server.js
@@ -5,6 +5,17 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
+const generateRandomString = () => {
+  let string = "";
+  const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  const length = 6;
+  for (let i = 0; i < length; i++) {
+    let randomNum = Math.floor(Math.random() * chars.length);
+    string += chars.substring(randomNum, randomNum + 1);
+  }
+  return string;
+};
+
 // generate unique shortURLID
 
 const urlDatabase = {
@@ -25,6 +36,19 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`)
+});
+
+
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id]
+  res.redirect(longURL);
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
@@ -38,22 +62,8 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-const generateRandomString = () => {
-  let string = "";
-  const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  const length = 6;
-  for (let i = 0; i < length; i++) {
-    let randomNum = Math.floor(Math.random() * chars.length);
-    string += chars.substring(randomNum, randomNum + 1);
-  }
-  return string;
-};
